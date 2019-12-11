f29677a95bd42c47c96d386ed447ad4241953558
Magali Champion <mchampion@wifi-pro-172-30-35-157.wifi.univ-paris5.fr>
2019-12-11 15:33:40 +0100

    code l1_spectral

--- l1_spectralclustering.R
+++ l1_spectralclustering.R
@@ -4,6 +4,17 @@ library(glmnet)
 l1_spectralclustering <- function(A,k){
   m <- ncol(A)
   
+  # 1 com
+  res <- find_com(A,k,m)
+  com <- res$v
+  
+  for (i in (1:k)){
+    res <- find_com(res$Adef,k,m) 
+    com <- cbind(com,res$v)
+  }
+}
+
+find_com <- function(A,k,m){
   # 1st step: SVD
   svd <- eigen(A) 
   U <- svd$vectors
@@ -12,11 +23,20 @@ l1_spectralclustering <- function(A,k){
   w <- U[1,(k+1):m]
   W <- t(U[-1,(k+1):m])
   
-  lambda <- cv.glmnet(W,w)
-  lambda <- lambda$lambda.min
-  v <- glmnet(W,w,lambda=lambda)
-  v <- c(1,v)
+  lassosol <- cv.glmnet(W,w)
+  if (lassosol$nzero[which(lassosol$lambda==lassosol$lambda.min)]==0){
+    Firstnnzero <- lassosol$lambda[which(lassosol$nzero>0)]
+    lambda_opt <- Firstnnzero[1]
+  } else {
+    lambda_opt <- lassosol$lambda.min
+  }
+  sol <- glmnet(W,w,lambda=lambda_opt)
+  sol <- sol$beta
+  
+  v <- c(1,as.matrix(sol))
   
   # 3rd step: find the other communities using deflation
+  A_def <- A - t(t(v)) %*% t(v)
   
+  return(list(A_def,v))
 }
