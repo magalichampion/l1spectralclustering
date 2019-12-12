2bbb851c2370d2fed14f61629652001b1c8d0f73
Magali Champion <mchampion@wifi-pro-172-30-35-157.wifi.univ-paris5.fr>
2019-12-12 15:55:24 +0100

    Nouveau code

--- l1_spectralclustering.R
+++ l1_spectralclustering.R
@@ -1,34 +1,41 @@
 # code l1 spectral clustering
 library(glmnet)
 
-# rendre propre le code (Aperturbed, ind)
-# regarder les sorties du lasso (trop restrictif? 10% du max-min)
-# shrinker quand?
 l1_spectralclustering <- function(A,k,index){
+  # inputs :
+  # - A : matrice d'adjacence
+  # - k : nombre de communautés
+  # - index : vecteur indiquant 1 noeud par communauté
   
-  # inputs : A, k, m, S1,...,Sk
+  # nombre de noeuds
   m <- ncol(A)
   
-  # 1 com
-  res <- find_com(A,k,m,index,1)
-  com <- res[[2]]
+  # 1st step: travail sur la 1ère communauté
+  res <- find_com(A,k,m,index,iteration=1)
+  com <- res$vector
   
+  # 2nd step: loop sur les communautés
   for (i in (2:k)){
-    res <- find_com(res[[1]],k,m,index,i) 
-    com <- cbind(com,res[[2]])
+    res <- find_com(res$A,k,m,index,iteration=i) 
+    com <- cbind(com,res$vector)
   }
+  
+  com[which(com<0.5)] <- 0
+  com[which(com>0.5)] <- 1
 }
 
-find_com <- function(A,k,m,index,i){
-  # 1st step: SVD
+find_com <- function(A,k,m,index,iteration){
+  # main code pour trouver les indices de chaque communauté
+  
+  # 1st step: SVD sur A 
   svd <- eigen(A) 
-  eigenvalues<- sort(svd$values,index.return=TRUE)
-  eigenvectors<- svd$vectors[,eigenvalues$ix]
-  U <- t(eigenvectors[,1:(n-k+i-1)])
+  eigenvalues <- sort(svd$values,index.return=TRUE)
+  eigenvectors <- svd$vectors[,eigenvalues$ix]
+  U <- t(eigenvectors[,1:(m-k+iteration-1)])
   
   # 2nd step: solve the lasso problem to find the first community
-  w <- U[,index[i]]
-  W <- U[,-index[i]]
+  w <- U[,index[iteration]]
+  W <- U[,-index[iteration]]
   
   #lassosol <- cv.glmnet(W,-w)
   # if (lassosol$nzero[which(lassosol$lambda==lassosol$lambda.min)]==0){
@@ -49,10 +56,11 @@ find_com <- function(A,k,m,index,i){
   sol <- glmnet(W,-w,lambda=0)
   sol <- sol$beta
   
-  solution=as.matrix(sol)
-  if(i==1){v=c( 1 ,solution[index[i]:length(solution)])}
-  else{
-    v=c(solution[1:(index[i]-1)], 1 ,solution[index[i]:length(solution)])
+  solution <- as.matrix(sol)
+  if(iteration==1){
+    v <- c( 1 ,solution[index[iteration]:length(solution)])
+  } else{
+    v <- c(solution[1:(index[iteration]-1)], 1 ,solution[index[iteration]:length(solution)])
   }
   #v <- rep(0,m)
   #v[index[i]] <- 1
@@ -61,5 +69,5 @@ find_com <- function(A,k,m,index,i){
   # 3rd step: find the other communities using deflation
   A_def <- A - t(t(v)) %*% t(v)
   
-  return(list(A=A_def,v=v))
+  return(list(A=A_def,vector=v))
 }
